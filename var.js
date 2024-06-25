29b2b4ae178e0f233070b3aef71ee35b1e9a841a
priyan2217 <162030924+priyan2217@users.noreply.github.com>
2024-06-25 23:11:11 +0530

    Update var.js

--- var.js
+++ var.js
@@ -1,7 +1,7 @@
 const accountId = 144553
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "Alwar"
 let accountState;
 
 // accountId = 2 // not allowed
